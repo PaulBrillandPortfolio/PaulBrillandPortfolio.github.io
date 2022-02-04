ced061566c2a81cc31bea6ff3b6565f7af637a17
PaulBrillandPortfolio <99006105+PaulBrillandPortfolio@users.noreply.github.com>
2022-02-04 01:53:22 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+$('input').on('change', function() {
+    $('body').toggleClass('blue');
+  });
+  
+// var hauteur = window.innerHeight;
+//   switch(hauteur) {
+//     case 0:
+//       document.querySelectorAll('**********').forEach(e => {
+//         e.addEventListener('*****', function () {
+//           document.getElementById('logo').style.background = "linear-gradient(to bottom, transparent 50%, #07eea9 30%)";
+//           })
+//         });
+//       break;
+//     case y:
+//       // code block
+//       break;
+//     default:
+//       // code block
+//   }
+
+//   logo
+
+
+//   background: linear-gradient(
+//     to bottom,
+//     transparent 50%,
+//     #07eea9 30%
+// );
